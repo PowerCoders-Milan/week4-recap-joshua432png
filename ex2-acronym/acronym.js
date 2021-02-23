d122c1c1c6318d76128b67539e0a6094ac010db2
Joshua Udeh <udehasilo@gmail.com>
2021-02-23 09:57:58 +0100

    e

--- ex2-acronym/acronym.js
+++ ex2-acronym/acronym.js
@@ -1,6 +1,11 @@
 // Rewrite the instructions in your own words
+// create a variable for all the acronyms
+// make each acronyms generate when given a particular phrase
+// all acronyms when generated must be in all caps
 
 // Write the objectives and the tests
+// create a list for all the acronyms with a given phrase
+// 
 
 // Write the pseudocode
 
